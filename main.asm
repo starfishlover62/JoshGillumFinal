f43430db6b2d84de37955a240cb546dc04565947
starfishlover62 <jmgillum@NIC.EDU>
2024-04-16 13:52:32 -0700

    Jumps to 16 bit calculator subroutine

--- main.asm
+++ main.asm
@@ -5,7 +5,7 @@
 
 ld r1, sixtyFourBit?
 brnp sixtyFourBit
-sixteenBit .fill x0 ; will jump to subroutine for 16 bit calculator
+sixteenBit jsr calculator16 ; will jump to subroutine for 16 bit calculator
 brnzp end
 sixtyFourBit .fill x0 ; will jump to subroutine for 64 bit calculator
 end halt
@@ -15,4 +15,14 @@ end halt
 .orig x3010 ; Settings
 sixtyFourBit? .fill x0000
 prettyPrint? .fill x0000
+.end
+
+
+.orig x3020
+calculator16:
+    ld r0, calculator16_offset
+    trap x21
+    ret
+    calculator16_offset .fill x41
+
 .end
