6b28eff3870c394234364fa43b3d2dd831d2517a
starfishlover62 <jmgillum@NIC.EDU>
2024-04-16 13:45:17 -0700

    Added check for bitmode of calculator

--- main.asm
+++ main.asm
@@ -3,12 +3,16 @@
 
 .orig x3000
 
-
-
+ld r1, sixtyFourBit?
+brnp sixtyFourBit
+sixteenBit .fill x0 ; will jump to subroutine for 16 bit calculator
+brnzp end
+sixtyFourBit .fill x0 ; will jump to subroutine for 64 bit calculator
+end halt
 
 .end
 
 .orig x3010 ; Settings
-64bit? .fill x0000
+sixtyFourBit? .fill x0000
 prettyPrint? .fill x0000
 .end
